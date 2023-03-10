cb4dde530d5e768857aeb6b70e7ea5287c156135
sergeygorbanev <gsv80@yandex.com>
2023-03-11 00:01:55 +0300

    Lesson 4 Tasks added

--- src/Lesson4/Main.java
+++ src/Lesson4/Main.java
@@ -3,23 +3,27 @@ package Lesson4;
 //import static Lesson4.PartOne.isIncludesNum;
 
 public class Main {
-    //    test array for Part 2
-    private static int[] ar = new int[] {1,1,1,3,3,1,3,1};
+//     test array for Part 1
+    private  static int[] arPart1 = {2,4,5,6,7,4,8,3};
+//    test array for Part 2
+    private static int[] arPart2 = new int[] {1,1,1,3,3,1,3,1};
     public static void main(String[] args) {
 //      Check Part One tasks:
 //        Task1.
         System.out.println("Task1");
 //        PartOne.printEven(99);
         System.out.println("=================");
+
 //        Task2.
         System.out.println("Task2");
-//        PartOne.multipleThreeFive(100);
+        PartOne.multipleThreeFive(100);
         System.out.println("=================");
 
 //        Task3.
         System.out.println("Task3");
 //        System.out.println(PartOne.checkSum());
         System.out.println("=================");
+
 //        Task4.
         System.out.println("Task4");
 //        System.out.println(PartOne.threeNumOrdered());
@@ -27,18 +31,18 @@ public class Main {
 
 //        Task5.
         System.out.println("Task5");
-//        System.out.println(PartOne.firstLastElThree());
+//        System.out.println(PartOne.firstLastElThree(arPart1));
         System.out.println("=================");
 
 //        Task6.
         System.out.println("Task6");
-//        System.out.println(PartOne.isIncludesNum());;
+//        System.out.println(PartOne.isIncludesNum(arPart1));
         System.out.println("=================");
 
         System.out.println("Part Two.");
 //        Task1.
         System.out.println("Task1");
-//        PartTwo.isSorted(ar);
+//        PartTwo.isSorted(arPart2);
         System.out.println("=================");
 
         System.out.println("Task2");
@@ -46,11 +50,11 @@ public class Main {
         System.out.println("=================");
 
         System.out.println("Task3");
-//        PartTwo.swapLastFirst(ar);
+//        PartTwo.swapLastFirst(arPart2);
         System.out.println("=================");
 
         System.out.println("Task4");
-        System.out.println(PartTwo.findUniq(ar));
+//        System.out.println(PartTwo.findUniq(arPart2));
         System.out.println("=================");
     }
 }
--- src/Lesson4/PartOne.java
+++ src/Lesson4/PartOne.java
@@ -16,27 +16,45 @@ public class PartOne {
 //  task 2
     public static void multipleThreeFive(int range){
 
-        List<Integer> listThree = new ArrayList<>();
-        List<Integer> listFive = new ArrayList<>();
-        List<Integer> listMultBoth = new ArrayList<>();
+        int countThree = 0;
+        int countFive = 0;
+        int countBoth = 0;
+        int[] arrThree = new int [range];
+        int[] arrFive =  new int [range];
+        int[] arrBoth =  new int [range];
 
-        for (int i = 1; i <= range ; i++) {
-            if(i%3 == 0 ){
-                listThree.add(i);
-                if(i%5 == 0){
-                    listMultBoth.add(i);
+        for (int i = 1; i <= range; i++) {
+            if (i % 3 == 0) {
+                arrThree[countThree]= i;
+                countThree++;
+                if (i % 5 == 0) {
+                    arrBoth[countBoth] = i;
+                    countBoth++;
                 }
-            } else if (i%5 == 0) {
-                listFive.add(i);
+            } else if (i % 5 == 0) {
+                arrFive[countFive]= i;
+                countFive++;
             }
         }
-        System.out.println("Числа кратные трем: " + listThree);
-        System.out.println("Числа кратные пяти :" + listFive);
-        System.out.println("Числа кратные трем и пяти: " + listMultBoth);
+        int[] arrThreeMod = new int[countThree];
+        for(int i=0; i<countThree; i++){
+            arrThreeMod[i] = arrThree[i];
+        }
+        int[] arrFiveMod = new int[countFive];
+        for(int i=0; i<countFive; i++){
+            arrFiveMod[i] = arrFive[i];
+        }
+        int[] arrBothMod = new int[countBoth];
+        for(int i=0; i<countBoth; i++){
+            arrBothMod[i] = arrBoth[i];
+        }
+        System.out.println("Числа кратные трем: " + Arrays.toString(arrThreeMod));
+        System.out.println("Числа кратные пяти :" + Arrays.toString(arrFiveMod));
+        System.out.println("Числа кратные трем и пяти: " + Arrays.toString(arrBothMod));
     }
 //  task 3
     public static boolean checkSum(){
-        boolean res = false;
+
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите первое число");
         int num1 = scn.nextInt();
@@ -45,12 +63,12 @@ public class PartOne {
         System.out.println("Введите третье число");
         int numSum = scn.nextInt();
         System.out.print("Результат: ");
-        res = (num1+num2 == numSum)? true : false;
-        return res;
+
+        return (num1+num2 == numSum)? true : false;
     }
 // task 4
     public static boolean threeNumOrdered(){
-        boolean res;
+
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите первое число");
         int num1 = scn.nextInt();
@@ -59,32 +77,28 @@ public class PartOne {
         System.out.println("Введите третье число");
         int num3= scn.nextInt();
         System.out.print("Результат: ");
-        res = (num1<num2 && num2<num3)? true : false;
-        return res;
+
+        return (num1<num2 && num2<num3)? true : false;
 
     }
 //    task 5
-    public static boolean firstLastElThree(){
-        boolean res;
-        int[] ar = {2,4,5,6,7,4,8,3};
-        System.out.println(Arrays.toString(ar));
-        res = (ar[0] == 3 || ar[ar.length-1] == 3)? true : false;
-        return res;
+    public static boolean firstLastElThree(int[] arrPart1){
+
+        System.out.println(Arrays.toString(arrPart1));
+        return (arrPart1[0] == 3 || arrPart1[arrPart1.length-1] == 3)? true : false;
     }
 
 //    task 6
-    public static boolean isIncludesNum(){
-        boolean isInc = false;
-        int[] ar = new int[] {0,51,2,21,5,5};
+    public static boolean isIncludesNum(int[] ar){
+
         int checkNum1 = 1;
         int checkNum2 = 3;
 
         for(int i=0; i< ar.length; i++){
             if(ar[i] == checkNum1 || ar[i] == checkNum2 ){
-                isInc = true;
-                return isInc;
+                return true;
             }
         }
-        return isInc;
+        return false;
     }
 }
